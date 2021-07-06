64013351e0a6fe0ba0b9ca25f36d6b9467a7a26a
ashikka <ashikagupta28@gmail.com>
2021-07-06 23:22:11 +0530

    feat: calling db function

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,16 @@
 /* eslint-disable import/no-unresolved */
 import express from 'express';
 import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
 import http from 'http';
 import logger from './utils/logger';
+import connectDB from './models/db';
 
 dotenv.config();
 
+connectDB(process.env.DB_URL || '');
+
 const app = express();
 const server = http.createServer(app);
-
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
-app.use(bodyParser.json());
 
 server.listen(process.env.PORT || 8000, (): void => {
   logger.info(`App listening on port ${process.env.PORT}`);
