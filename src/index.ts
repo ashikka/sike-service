e34eeea3f298761f92d568690231b3f2aecceef0
ashikka <ashikagupta28@gmail.com>
2021-07-09 20:57:01 +0530

    feat: router

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import http from 'http';
 import logger from './utils/logger';
 import connectDB from './models/db';
+import routes from './routes/mainRouter';
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ connectDB(process.env.DB_URL || '');
 
 const app = express();
 const server = http.createServer(app);
+
+app.use('/', routes);
 
 server.listen(process.env.PORT || 8000, (): void => {
   logger.info(`App listening on port ${process.env.PORT}`);
