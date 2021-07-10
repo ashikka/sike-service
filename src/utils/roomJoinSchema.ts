52224f9dc9df7ace650365202ba3709a886d8827
ashikka <ashikagupta28@gmail.com>
2021-07-10 15:34:58 +0530

    fix: typo

--- src/utils/roomJoinSchema.ts
+++ src/utils/roomJoinSchema.ts
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 const roomJoinSchema = Joi.object({
   username: Joi.string().required(),
-  roomID: Joi.string().required(),
+  roomId: Joi.string().required(),
 });
 
 export default roomJoinSchema;
