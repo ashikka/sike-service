7cd671fce75cb4e802e09b18a704883384a5ae73
ashikka <ashikagupta28@gmail.com>
2021-08-07 23:25:20 +0530

    feat: deployment test route

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -1,7 +1,14 @@
-import express from 'express';
+import express, { Response, Request } from 'express';
 import roomRouter from './room';
 
 const router = express.Router();
+
+router.get('/', async (req: Request, res: Response) => {
+  res.json({
+    success: true,
+    message: 'Welcome to sike-service',
+  });
+});
 
 router.use('/room', roomRouter);
 
